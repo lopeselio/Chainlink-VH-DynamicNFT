b4c0b123fb9d2d7fd17b4c7fc2552fa1c632e784
lopeselio <lopeselio@gmail.com>
2021-03-22 15:34:19 +0530

    fixing migration

--- migrations/3_NFT_simple.js
+++ migrations/3_NFT_simple.js
@@ -1,10 +1,7 @@
 const NFTSimple = artifacts.require('NFTSimple')
-// const { LinkToken } = require('@chainlink/contracts/truffle/v0.4/LinkToken')
 
 module.exports = async (deployer, network, [defaultAccount]) => {
-    // Local (development) networks need their own deployment of the LINK
-    // token and the Oracle contract
-    
+
     deployer.deploy(NFTSimple)
     
 }
--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,11 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 require('dotenv').config();
+const Web3 = require("web3");
 // require('babel-polyfill');
 // require('babel-register');
 
-const mnemonic = process.env.MNEMONIC
-const url = process.env.RPC_URL
+const mnemonicPhrase = process.env.MNEMONIC
+const Url = process.env.RPC_URL
 
 module.exports = {
   networks: {
@@ -20,19 +21,28 @@ module.exports = {
     },
     kovan: {
       provider: () => {
-        return new HDWalletProvider(mnemonic, url)
+        return new HDWalletProvider(mnemonic, providerOrUrl, 2)
       },
       network_id: '42',
       skipDryRun: true
     },
     rinkeby: {
-      provider: () => {
-        return new HDWalletProvider(mnemonic, url)
-      },
-      network_id: '4',
-      skipDryRun: true,
-      networkCheckTimeout: 999999
-    },
+      provider: () => 
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: mnemonicPhrase
+          },
+          providerOrUrl: Url,  
+          // numberOfAddresses: 3,
+          addressIndex: 2,   
+          // skipDryRun: true,
+          shareNonce: true,
+          pollingInterval: 8000
+          }),
+        network_id: '4',
+        // networkCheckTimeout: 999999,
+
+      }
   },
   compilers: {
     solc: {
@@ -44,7 +54,7 @@ module.exports = {
     }
   },
   api_keys: {
-    etherscan: process.env.ETHERSCAN_API_KEY
+    etherscan: process.env.ETHERSCAN_API_KEY,
   },
   plugins: [
     'truffle-plugin-verify'
